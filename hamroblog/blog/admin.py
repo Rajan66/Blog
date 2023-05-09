abf279cece632d9a890f9440a0c006d6ff2f25bb
Rajan66 <rajanmaharjan042@gmail.com>
2023-05-10 01:23:53 +0545

    Added Post Model(admin can add posts)

--- hamroblog/blog/admin.py
+++ hamroblog/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+# This makes the blog post entries accessible from the admin area
+admin.site.register(Post)
--- hamroblog/blog/models.py
+++ hamroblog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+# Imports User that we created (admin) also other common users 
 # Create your models here.
+
+class Post(models.Model):
+    # form in django
+    title = models.CharField(max_length=225)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # on_delete -  deletes all the posts when the user account is deleted
+    body = models.TextField()
+    
+    def __str__(self):
+        # To view the title and author of the post in the admin area 
+        # There will be a list of posts so it will be much straightforward
+        return self.title + ' | ' + str(self.author)
+        # self.author is a Object so need to parse to String 
