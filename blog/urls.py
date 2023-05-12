d41959516ae22d057cb3573f85406a4224f2b743
Rajan Maharjan <rajanmaharjan042@gmail.com>
2023-05-12 12:03:57 +0545

    Minor changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import HomeView
+from .views import HomeView,ArticleView
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
+    path('article/<int:pk>',ArticleView.as_view(),name='article-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,8 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class ArticleView(DetailView):
+    model = Post
+    template_name = 'articles_detail.html'
