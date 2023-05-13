d5e411a2582a01ee2d7196397d8fc7cac15c277d
Rajan Maharjan <rajanmaharjan042@gmail.com>
2023-05-13 13:30:56 +0545

    Added add post file

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import HomeView,ArticleDetailView
+from .views import HomeView,ArticleDetailView,AddPostView
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
     path('article/<int:pk>',ArticleDetailView.as_view(),name='article-details'),
     # int:pk:- to reference the primary of the particular blog post
+    path('add_post/',AddPostView.as_view(),name='add-post'),
 ]
