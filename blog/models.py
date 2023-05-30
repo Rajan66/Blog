e53389f290af83d31939e02144c2ab10c459fe79
Rajan66 <rajanmaharjan042@gmail.com>
2023-05-30 16:10:00 +0545

    Add Post

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Imports User that we created (admin) also other common users 
 # Create your models here.
 
 class Post(models.Model):
     # form in django
     title = models.CharField(max_length=225)
+    title_tag = models.CharField(max_length=255,default="A Beautiful Blog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # on_delete -  deletes all the posts when the user account is deleted
     body = models.TextField()
@@ -15,3 +17,8 @@ class Post(models.Model):
         # There will be a list of posts so it will be much straightforward
         return self.title + ' | ' + str(self.author)
         # self.author is a Object so need to parse to String 
+        
+    def get_absolute_url(self):
+        # 
+        return reverse("article-details", args=(str(self.id)))
+    
