de94f86db41f02df7460ebb9573c3bfd115e51f8
Rajan66 <rajanmaharjan042@gmail.com>
2023-06-01 22:37:48 +0545

    Urls and Views created for Update Post

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 class Post(models.Model):
     # form in django
     title = models.CharField(max_length=225)
+    # title tag: Title at the top of the html page
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # on_delete -  deletes all the posts when the user account is deleted
