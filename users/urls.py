46c8ab1119604518ea863a8f2011062cccd5ba06
Rajan66 <rajanmaharjan042@gmail.com>
2023-06-07 22:30:31 +0545

    Added user auth

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import UserRegisterView
 
 
 urlpatterns = [
-    
+    path('register/',UserRegisterView.as_view(), name='register'),
+    # no need to make login url because the django.contrib.auth.urls automatically provides us with the login url.
 ]
