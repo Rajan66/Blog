9c038553dad157a7112e83b7347288c938521240
Rajan66 <rajanmaharjan042@gmail.com>
2023-06-13 22:45:21 +0545

    Added Category Page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView,DeletePostView,AddCategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -8,6 +8,9 @@ urlpatterns = [
     # int:pk:- to reference the primary of the particular blog post
     path('add_post/', AddPostView.as_view(), name='add-post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='edit-post'),
+    # /remove : just so that the url is different and does not cause any err; url name can be anything here
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete-post'),
     path('add_category/', AddCategoryView.as_view(), name='add-category'),
+    # str as the category name isn't an int like in the prev urls..
+    path('category/<str:cat>/', CategoryView, name="category"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post,Category
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -9,6 +9,8 @@ from django.urls import reverse_lazy
 # def home(request):
 #     context = {}
 #     return render(request, 'home.html', context)
+# Class based views queries the database for us
+# In functional view however we need to do it by ourselves
 class HomeView(ListView):
     # ListView to view all the blog posts in the database
     model = Post
@@ -40,6 +42,7 @@ class AddCategoryView(CreateView):
     template_name = 'add_category.html'
     fields = ['name']
 
+
 class UpdatePostView(UpdateView):
     # UpdateView to update fields in the blog post
     model = Post
@@ -51,3 +54,15 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
+
+# this is a functional view rather than a class based view
+# this is useful here because we can pass the request and the category as paramter
+# category meaning sports, entertainment, music or other categories that we have stored in the database
+
+
+def CategoryView(request, cat):
+    # This filters all the post of the specific category ie. cat (which ever is passed in the url)
+    # Post meaning the Post table(model)
+    post_category = Post.objects.filter(category=cat)
+    context = {'cat': cat, 'post_category': post_category}
+    return render(request, 'categories.html', context)
