d19fdaa5bef4f60b805af66a1aab619df3882784
Rajan66 <rajanmaharjan042@gmail.com>
2023-08-02 14:50:05 +0545

    Flash messages added on login/register

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import registerPage,loginPage,UserEditView,PasswordView,ShowProfilePageView,EditProfilePageView,CreateProfilePageView
+from .views import registerPage,loginPage,logoutPage,UserEditView,PasswordView,ShowProfilePageView,EditProfilePageView,CreateProfilePageView
 from . import views
 
 urlpatterns = [
     path('register/',registerPage, name='register'),
     path('login/',loginPage, name='login'),
-    # no need to make login url because the django.contrib.auth.urls automatically provides us with the login url.
+    path('logout/',logoutPage, name='logout'),
     path('edit_profile/',UserEditView.as_view(), name='edit-profile'),
     path('password/',PasswordView.as_view(), name='password'),
     path('password_success/',views.password_success, name='password-success'),  
--- users/views.py
+++ users/views.py
@@ -12,11 +12,6 @@ from blog.models import Category, Profile
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
-# class UserRegisterView(generic.CreateView):
-#     form_class = UserCreationForm
-#     template_name = 'registration/register.html'
-#     success_url = reverse_lazy('login')
-
 
 @csrf_exempt
 def registerPage(request):
@@ -38,17 +33,22 @@ def registerPage(request):
 
 
 def loginPage(request):
-    if request.method == "post":
+    if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            redirect('home')
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect!')
     context = {}
     return render(request, 'registration/login.html', context)
 
+def logoutPage(request):
+    logout(request)
+    return redirect('home')
 
 class UserEditView(UpdateView):
     form_class = EditProfileForm
